3c5ca67154101891b927671377ffb73403fb0f88
KHAAdotPK <spysee.pk@gmail.com>
2025-03-14 23:04:48 +0500

    feat: add CLEAN.cmd and PULL.cmd scripts; update argsv Cargo.toml path
    - Added CLEAN.cmd to clean build directories and files for a fresh workspace.
    - Added PULL.cmd to clone required repositories for dependency setup.
    - Updated the path in argsv/Cargo.toml to reflect the correct location for external dependency usage.

---
+++ CLEAN.cmd
@@ -0,0 +1,17 @@
+rmdir /S /Q start.dir
+rmdir /S /Q clap.dir 
+rmdir /S /Q x64
+rmdir /S /Q Debug
+del ALL_BUILD.vcxproj
+del ALL_BUILD.vcxproj.filters
+del clap.vcxproj
+del clap.vcxproj.filters
+del CMakeCache.txt
+del make_install.cmake
+del start.vcxproj
+del start.vcxproj.filters
+del ZERO_CHECK.vcxproj
+del ZERO_CHECK.vcxproj.filters
+del cmake_install.cmake
+del metaverse-tools-editor.sln
+
